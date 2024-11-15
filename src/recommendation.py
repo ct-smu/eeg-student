7eeaf879f8859fda76f489476ddb3f4d349c2b7d
lhw4846 <lhw4846@naver.com>
2024-11-15 11:43:39 +0900

    Initial commit

--- src/recommendation.py
+++ src/recommendation.py
@@ -247,6 +247,48 @@ def recommend_select(
     image_folder: str,
 ) -> None:
     max_values_per_channel = []
+    for channel_idx in range(len(channels)):
+        max_values = []
+        for num_images in range(len(times_list)):
+            selected_indices = [
+                index
+                for index, value in enumerate(times_list[num_images])
+                if 0.1 <= value <= 0.5
+            ]
+            start_index = selected_indices[0]
+            end_index = selected_indices[-1]
+
+            max_value = max(
+                avg_evoked_list[num_images][channel_idx][start_index : end_index + 1]
+            )
+            max_values.append(max_value)
+        max_values_per_channel.append(max_values)
+
+    indices_of_largest_values_per_channel = []
+    for channel in range(len(max_values_per_channel)):
+        indices_of_largest_values = sorted(
+            range(len(max_values_per_channel[channel])),
+            key=lambda i: max_values_per_channel[channel][i],
+            reverse=True,
+        )[:3]
+        largest_values = [
+            max_values_per_channel[channel][i] for i in indices_of_largest_values
+        ]
+        top_values_and_indices = [
+            (value, index)
+            for value, index in zip(largest_values, indices_of_largest_values)
+        ]
+        indices_of_largest_values_per_channel.append(top_values_and_indices)
+
+    top_values_and_indices = sum(indices_of_largest_values_per_channel, [])
+    sorted_top_values_and_indices = sorted(
+        top_values_and_indices, key=lambda i: i[0], reverse=True
+    )
+    top_index = sorted_top_values_and_indices[0][1]
+    print(f"your selection is {top_index*2+1}")
+    image_filename = f"{image_folder}/{top_index*2+1}.png"
+    image = Image.open(image_filename)
+    image.show()
 
 
 def recommend_speller(
